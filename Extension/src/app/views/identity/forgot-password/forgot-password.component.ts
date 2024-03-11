0458b0bba3e7b607ea40a3f977523105464b4dac
charles_jennings <charlesjennings@zoho.com>
2024-03-11 17:13:13 -0500

    Added the action page

--- Extension/src/app/views/identity/forgot-password/forgot-password.component.ts
+++ Extension/src/app/views/identity/forgot-password/forgot-password.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { BasePageDirective } from '../../shared/base-page.directive';
 import { Title } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
